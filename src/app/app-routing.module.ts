34c653c71accc55ce0815fef469e1010c5a5837f
Jesus Garizao <jesudgm.11@outlook.com>
2022-07-21 19:07:11 -0500

    Terminacion de estructura basica y de configuracion de estilo de angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', loadChildren: () => import('./pages/public/auth/login/login.module').then(m => m.LoginModule)},
+  {path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)}
 ];
 
 @NgModule({
--- src/app/pages/private/private.module.ts
+++ src/app/pages/private/private.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
 import { HomeComponent } from './home/home.component';
+import {HomeModule} from "./home/home.module";
 
 
 @NgModule({
   declarations: [
-  
+
     HomeComponent
   ],
   imports: [
     CommonModule,
-    PrivateRoutingModule
+    PrivateRoutingModule,
+    HomeModule
   ]
 })
 export class PrivateModule { }
