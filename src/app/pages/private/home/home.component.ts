458c1fa199e11310d19d2fa75b056ab5db174470
Jesus Garizao <tecnoaprendizsoftg9@gmail.com>
2022-07-25 12:45:36 -0500

    Correccion en el estilo del home

--- src/app/pages/private/home/home.component.ts
+++ src/app/pages/private/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,20 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class HomeComponent {
 
+  usuario="Jesus Garizao"
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  ngOnInit(): void {
+    Swal.fire({
+      icon: 'success',
+      title: 'Welcome',
+    })
+  }
 }
--- src/app/pages/private/private.module.ts
+++ src/app/pages/private/private.module.ts
@@ -9,11 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatMenuModule} from "@angular/material/menu";
 
 
 @NgModule({
   declarations: [
-  
+
     HomeComponent
   ],
   imports: [
@@ -25,6 +26,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatMenuModule,
   ]
 })
 export class PrivateModule { }
