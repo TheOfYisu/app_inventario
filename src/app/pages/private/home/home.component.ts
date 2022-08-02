73b573decad9548c7e33cbfb8e5bf9348405b511
Jesus Garizao <jesudgm.11@outlook.com>
2022-08-01 21:19:34 -0500

    ccambios en el estilo del home

--- src/app/pages/private/home/home.component.ts
+++ src/app/pages/private/home/home.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import Swal from 'sweetalert2';
+import { navbarData } from './nav-data';
 
 @Component({
   selector: 'app-home',
@@ -11,15 +12,10 @@ import Swal from 'sweetalert2';
 })
 export class HomeComponent {
 
-  usuario="Jesus Garizao"
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
-  expandedIndex = 0;
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  collapsed=true;
+  navData = navbarData;
+
+  constructor() {}
 
   ngOnInit(): void {
     Swal.fire({
