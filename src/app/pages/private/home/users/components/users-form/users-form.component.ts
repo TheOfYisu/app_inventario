db88baf015ce517ad095f26a487b8b23b596c045
Jesus Garizao <jesudgm.11@outlook.com>
2022-09-17 15:15:32 -0500

    fix error form edit

--- src/app/pages/private/home/users/components/users-form/users-form.component.ts
+++ src/app/pages/private/home/users/components/users-form/users-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Users_Service} from 'src/app/core/services/users.service';
-import { Users_interface} from 'src/app/interfaces/users';
+import {Users_interface} from 'src/app/interfaces/users';
 import Swal from "sweetalert2";
 
 @Component({
@@ -9,35 +9,36 @@ import Swal from "sweetalert2";
   templateUrl: "./users-form.component.html",
   styleUrls: ['./users-form.component.css']
 })
-export class UsersFormComponent implements OnInit{
-  formuser:FormGroup
-  isEdit=false
+export class UsersFormComponent implements OnInit {
+  formuser: FormGroup
+  isEdit = false
 
   constructor(
-    private fb:FormBuilder,
-    private Users_Service:Users_Service,
+    private fb: FormBuilder,
+    private Users_Service: Users_Service,
   ) {
-    this.formuser=this.fb.group({
-      name:['',Validators.required],
-      lastname:['',Validators.required],
-      email:['',Validators.required],
-      password:['',Validators.required],
-      checkpassword:['']
+    this.formuser = this.fb.group({
+      id:[''],
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      checkpassword: ['']
     })
   }
 
-  save_users(){
-    const adduser:Users_interface={
-      id:Math.floor(Math.random() * 100),
-      name:this.formuser.value.name,
-      lastname:this.formuser.value.lastname,
-      email:this.formuser.value.email,
-      password:this.formuser.value.password,
-      checkpassword:this.formuser.value.checkpassword
+  save_users() {
+    const adduser: Users_interface = {
+      id:this.formuser.value,
+      name: this.formuser.value.name,
+      lastname: this.formuser.value.lastname,
+      email: this.formuser.value.email,
+      password: this.formuser.value.password,
+      checkpassword: this.formuser.value.checkpassword
     }
-    console.log(adduser)
-    if (adduser.password==adduser.checkpassword){
-      if(this.isEdit==false){
+    if (this.isEdit == false) {
+      adduser.id=Math.floor(Math.random() * 100)
+      if (adduser.password == adduser.checkpassword) {
         Swal.fire({
           title: 'Are you sure?',
           icon: 'warning',
@@ -49,29 +50,30 @@ export class UsersFormComponent implements OnInit{
             this.Users_Service.adduser(adduser)
           }
         })
-      }else{
-        Swal.fire({
-          title: 'Are you sure?',
-          text: "You won't be able to revert this!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Yes, changed it!',
-          toast: true,
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.Users_Service.updateuser(adduser);
-            Swal.fire(
-              'Susefull!',
-              'Your file has been changed.',
-              'success'
-            )
-          }
-        })
+      } else {
+        alert("Las contraseñas no son las mismas")
       }
-    }else{
-      alert("Las contraseñas no son las mismas")
+    } else {
+      adduser.id=this.formuser.value.id
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, changed it!',
+        toast: true,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.Users_Service.updateuser(adduser);
+          Swal.fire(
+            'Susefull!',
+            'Your file has been changed.',
+            'success'
+          )
+        }
+      })
     }
 
   }
@@ -79,6 +81,7 @@ export class UsersFormComponent implements OnInit{
   ngOnInit(): void {
     this.Users_Service.editUser$.subscribe((data) => {
       this.isEdit = !!data?.id;
+      this.formuser.controls['id'].disable()
       this.formuser.patchValue(data)
     });
   }
