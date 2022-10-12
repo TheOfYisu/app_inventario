5e6332a054f6fb38b4074abf99c24de342910693
Sebas15897 <jcontreras@transportationamerica.com>
2022-10-11 21:33:29 -0500

    feat: update all changes

---
+++ src/app/core/enums/inventory.enum.ts
@@ -0,0 +1,5 @@
+export const enum INVENTORY_ENUM {
+  "COMPUTERS" = 1,
+  "DISPLAY" = 2,
+  "KEYBOARDS" = 3,
+}
--- src/app/pages/private/home/inventory/component/inventory-form/inventory-form.component.ts
+++ src/app/pages/private/home/inventory/component/inventory-form/inventory-form.component.ts
@@ -1,24 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
-import {InventoryService} from "../../../../../../core/services/inventory.service";
-import Swal from "sweetalert2";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { InventoryService } from '../../../../../../core/services/inventory.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inventory-form',
   templateUrl: './inventory-form.component.html',
-  styleUrls: ['./inventory-form.component.css']
+  styleUrls: ['./inventory-form.component.css'],
 })
 export class InventoryFormComponent implements OnInit {
   select_device: FormGroup;
   form_general: FormGroup;
-  form_computer: FormGroup;
-  form_additional: FormGroup;
   devicesellec: string;
   checked = false;
-  valitbutton = !false
+  valitbutton = !false;
 
   devices = [
+    {
+      id: 1,
+      description: 'Computer',
+    },
+    {
+      id: 2,
+      description: 'Display',
+    },
+    {
+      id: 3,
+      description: 'Keyboards',
+    },
+    {
+      id: 4,
+      description: 'Mouses',
+    },
+    {
+      id: 5,
+      description: 'Ups',
+    },
+    {
+      id: 6,
+      description: 'Home Appliances',
+    },
+  ];
+
+  /* devices = [
     {value: 'Computer'},
     {value: 'Display'},
     {value: 'Keyboards'},
@@ -31,32 +56,28 @@ export class InventoryFormComponent implements OnInit {
     {value: 'Printer'},
     {value: 'Phone Cooperative'},
     {value: 'Other'}
-  ]
+  ] */
 
-  status = [
-    {value: 'Available'},
-    {value: 'In Use'},
-    {value: 'Damaged'},
-  ]
+  status = [{ value: 'Available' }, { value: 'In Use' }, { value: 'Damaged' }];
 
   locations = [
-    {value: 'OFFI 1010'},
-    {value: 'OFFI 1001'},
-    {value: 'OFFI 213'},
-  ]
+    { value: 'OFFI 1010' },
+    { value: 'OFFI 1001' },
+    { value: 'OFFI 213' },
+  ];
 
   operations = [
-    {value: 'Centrum'},
-    {value: 'Transportation'},
-    {value: 'Soft-g.NET'}
-  ]
+    { value: 'Centrum' },
+    { value: 'Transportation' },
+    { value: 'Soft-g.NET' },
+  ];
 
   networks_devices = [
-    {value: 'Switch'},
-    {value: 'Router'},
-    {value: 'Biometrico'},
-    {value: 'Modem'}
-  ]
+    { value: 'Switch' },
+    { value: 'Router' },
+    { value: 'Biometrico' },
+    { value: 'Modem' },
+  ];
 
   constructor(
     private router: Router,
@@ -66,22 +87,76 @@ export class InventoryFormComponent implements OnInit {
     this.createForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeDevice(device: string) {
+    this.devicesellec = device;
+    const controlsComputer = [
+      'brand_proccessor',
+      'model_proccessor',
+      'type_ram',
+      'capacy_ram',
+      'type_storage',
+      'capacy_storage',
+    ];
+    switch (device) {
+      case 'Computer':
+        controlsComputer.forEach((control: string) => {
+          if (control) {
+            this.setRequired(control);
+          }
+        });
+        break;
+      case 'Display':
+        controlsComputer.forEach((control: string) => {
+          if (control) {
+            this.setNotRequired(control);
+          }
+        });
+        break;
+      case 'Networks':
+        controlsComputer.forEach((control: string) => {
+          if (control) {
+            this.setNotRequired(control);
+          }
+        });
+        break;
+      case 'Home Appliances':
+        controlsComputer.forEach((control: string) => {
+          if (control) {
+            this.setNotRequired(control);
+          }
+        });
+        break;
+      default:
+        controlsComputer.forEach((control: string) => {
+          if (control) {
+            this.setNotRequired(control);
+          }
+        });
+        break;
+    }
   }
 
+  setRequired(control: string) {
+    if (control) {
+      this.form_general.get(control).setValidators(Validators.required);
+      this.form_general.updateValueAndValidity();
+    }
+  }
 
-  form(forms: string) {
-    this.devicesellec = forms
-    this.form_general.reset()
-    this.form_computer.reset()
-    this.form_additional.reset()
+  setNotRequired(control: string) {
+    if (control) {
+      this.form_general.get(control).clearValidators();
+      this.form_general.get(control).disable();
+      this.form_general.get(control).setValue(null);
+      this.form_general.updateValueAndValidity();
+    }
   }
 
   createForm() {
-    this.select_device = this.fb.group({
-      select_devices: [null, Validators.required]
-    })
     this.form_general = this.fb.group({
+      select_devices: [null, Validators.required],
       internal_serial: [null, Validators.required],
       brand: [null, Validators.required],
       model: [null, Validators.required],
@@ -93,40 +168,50 @@ export class InventoryFormComponent implements OnInit {
       unit_value: [null, Validators.required],
       electronic_invoice: [null, Validators.required],
       supplier: [null, Validators.required],
+      brand_proccessor: null,
+      model_proccessor: null,
+      type_ram: null,
+      capacy_ram: null,
+      type_storage: null,
+      capacy_storage: null,
+      brand_carger: null,
+      serial_carger: null,
+      havecharger: null
     });
-    this.form_computer = this.fb.group({
-      brand_proccessor: [null, Validators.required],
-      model_proccessor: [null, Validators.required],
-      type_ram: [null, Validators.required],
-      capacy_ram: [null, Validators.required],
-      type_storage: [null, Validators.required],
-      capacy_storage: [null, Validators.required]
-    })
-    this.form_additional = this.fb.group({
-      brand_carger: [null],
-      serial_carger: [null]
-    })
   }
 
   get seletdevice(): boolean {
-    return this.select_device.get('select_devices').value;
+    return this.form_general.get('select_devices').value;
+  }
+
+  get formIsValid(): boolean {
+    return this.form_general.valid;
   }
 
   savedevice() {
-    if (this.select_device.get('select_devices').value == 'Computer') {
-      if (this.form_general.valid && this.form_computer.valid) {
-        this.InventoryService.adddevice_computer(this.form_general.value, this.form_computer.value,this.form_additional.value)
-      } else {
-        alert("faltan datos")
-      }
-    }else if (this.select_device.get('select_devices').value == 'Display') {
+    const formData = Object.assign({}, this.form_general.getRawValue());
+    console.log(formData)
+    /* if (formData.select_devices === 'Computer') {
       if (this.form_general.valid) {
-        this.InventoryService.adddevice_display(this.form_general.value,this.form_additional.value)
+        this.InventoryService.adddevice_computer(
+          this.form_general.value,
+          this.form_computer.value,
+          this.form_additional.value
+        );
       } else {
-        alert("faltan datos")
+        alert('faltan datos');
       }
-    }else {
-      this.InventoryService.adddevice_general(this.form_general.value)
-    }
+    } else if (this.select_device.get('select_devices').value == 'Display') {
+      if (this.form_general.valid) {
+        this.InventoryService.adddevice_display(
+          this.form_general.value,
+          this.form_additional.value
+        );
+      } else {
+        alert('faltan datos');
+      }
+    } else {
+      this.InventoryService.adddevice_general(this.form_general.value);
+    } */
   }
 }
